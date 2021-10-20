0d9420c087fc4aa0c0e40f71630ebf9e2388682a
fszilard01 <fekete.szilard76@gmail.com>
2021-10-20 14:17:26 +0200

    add HelloOthers

---
+++ week-02/java/src/ez/az/HelloOthers.java
@@ -0,0 +1,13 @@
+package ez.az;
+
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Péter!");
+        System.out.println("Hello Attila!");
+        System.out.println("Hello Dora!");
+
+    }
+}
+
+
