c123bc5b8e0532ccc3c48dd8e2c1890122165342
fszilard01 <fekete.szilard76@gmail.com>
2021-10-20 14:00:51 +0200

    add LipemLopom

---
+++ week-02/java/src/ez/az/LipemLopom.java
@@ -0,0 +1,13 @@
+package ez.az;
+
+public class LipemLopom {
+    public static void main(String[] args) {
+        System.out.println("Lipem, lopom a szőlőt,");
+        System.out.println("elaludt az öreg csősz.");
+        System.out.println("Furkósbot a kezébe,");
+        System.out.println("vaskalap a fejébe.");
+
+    }
+}
+
+
