35f79bab76331bf768e0882a8bb754e006b62210
fszilard01 <fekete.szilard76@gmail.com>
2021-10-20 17:57:27 +0200

    add IntroduceYourself

---
+++ week-02/java/src/Types/IntroduceYourself.java
@@ -0,0 +1,28 @@
+package Types;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiír rólad pár részletet a konzolra
+        // Minden egyes részletet új sorba írja a program:
+        //  - Neved
+        //  - Korod
+        //  - Magasságod méterben kifejezve (tört érték)
+        //
+        //  Példa kimenet (output):
+        //  John Doe
+        //  31
+        //  1.87
+
+        String name = "Fekete Szilárd";
+        String age = "45";
+        String height = "1.90";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+
+    }
+
+}
+
+
