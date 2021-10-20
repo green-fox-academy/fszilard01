8d32e5baa0b5fa4a2134d2e2c8997406b23fe935
fszilard01 <fekete.szilard76@gmail.com>
2021-10-20 18:39:20 +0200

    add FavoriteNumber

---
+++ week-02/java/src/Variables/FavoriteNumber.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 8;
+        // Írd át a fenti számot a kedvenc számoddá
+        // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
+        // Kiíratáskor használd fel a favoriteNumber változót
+        System.out.println("Az én kedvenc számom: " + favoriteNumber);
+    }
+}
+
