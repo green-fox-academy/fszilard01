79b1870942670fd05a8dee72fdf62c21edfefcee
fszilard01 <fekete.szilard76@gmail.com>
2021-10-21 08:43:03 +0200

    add IWontCheatOnTheExams

---
+++ week-02/java/src/Loops/IWontCheatOnTheExams.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        // Írj egy programot ami 100-szor kiírja:
+        // "Nem fogok csalni az a vizsgán!"
+
+        int a = 0;
+        for (int i = 0; i < 100; i++){
+            System.out.println("Nem fogok csalni az a vizsgán!");
+        }
+    }
+}
+
