f5a85d78cbee9db9852b54fd9901627f9c92f99f
fszilard01 <fekete.szilard76@gmail.com>
2021-10-21 19:33:30 +0200

    add PrintEven

---
+++ week-02/java/src/Loops/PrintEven.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class PrintEven {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiírja az összes páros számot 0 és 500 között
+
+
+        int a = 0;
+        for (int i = 0; i < 501; i +=2){
+            System.out.println(i);
+        }
+    }
+
+}
