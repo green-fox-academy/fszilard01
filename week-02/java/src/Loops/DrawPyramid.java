46e9180ca03694eaeaa5876bc3c0c30897bd7ae6
fszilard01 <fekete.szilard76@gmail.com>
2021-10-22 20:09:04 +0200

    add DrawPyramidChange

--- week-02/java/src/Loops/DrawPyramid.java
+++ week-02/java/src/Loops/DrawPyramid.java
@@ -1,15 +1,4 @@
 package Loops;
-// Írj egy programot, ami egy számot kér a standard input-ból és egy
-// ilyen piramist rajzol:
-//
-//
-//    *
-//   ***
-//  *****
-// *******
-//
-// A piramisnak annyi sora legyen,
-// mint amennyi az inputból kiolvasott szám értéke
 
 import java.util.Scanner;
 
@@ -18,8 +7,6 @@ public class DrawPyramid {
 
         Scanner scanner = new Scanner(System.in);
         int userInput = scanner.nextInt();
-
-
 
         for (int i = 0; i < userInput; i++) {
             for (int j = (userInput - 1) - i; j > 0; j--) {
@@ -31,17 +18,9 @@ public class DrawPyramid {
             for (int y = 1; y <= i; y++) {
                 System.out.print("*");
             }
-
             System.out.print("\n");
         }
-
-
-
-
-
-        }
-
-
     }
+}
 
 
