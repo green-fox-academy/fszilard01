c088aa9b363fa5a832259902f213c13eda350ed0
fszilard01 <fekete.szilard76@gmail.com>
2021-10-23 20:31:07 +0200

    add Third

---
+++ week-02/java/src/Arrays/Third.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+public class Third {
+    public static void main(String[] args) {
+
+        int[] third = {4, 5, 6, 7};
+        System.out.println("A tömb harmadik eleme: " + third[2]);
+    }
+}
