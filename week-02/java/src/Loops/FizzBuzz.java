00b102a4976ea0ba40284515d018ced9f343b4fa
fszilard01 <fekete.szilard76@gmail.com>
2021-10-23 12:55:36 +0200

    add FizzBuzz

--- week-02/java/src/Loops/FizzBuzz.java
+++ week-02/java/src/Loops/FizzBuzz.java
@@ -1,2 +1,19 @@
-package Loops;public class FizzBuzz {
+package Loops;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 15 > 0){
+                System.out.println("Fizz");
+            }else if (i % 5 == 0 && i % 15 > 0){
+                System.out.println("Buzz");
+            }else if (i % 15 == 0){
+                System.out.println("FizzBuzz");
+            }else {
+            System.out.println(i);}
+
+        }
+
+    }
 }
