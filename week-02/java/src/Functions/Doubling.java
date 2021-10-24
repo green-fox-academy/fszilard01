5d944d9d63a2dd2c35b8e4e091aebd568bce956f
fszilard01 <fekete.szilard76@gmail.com>
2021-10-24 19:46:35 +0200

    add Doubling

---
+++ week-02/java/src/Functions/Doubling.java
@@ -0,0 +1,14 @@
+package Functions;
+
+public class Doubling {
+    public static void main(String[] args) {
+        
+        int baseNum = 123;
+        System.out.println(doubling(baseNum));
+    }
+
+    public static int doubling(int dual) {
+        return dual * 2;
+    }
+}
+
