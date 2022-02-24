dbb4850ee2d887fb141a66bbc1a9c9b365e1e4a5
fszilard01 <fekete.szilard76@gmail.com>
2022-02-24 10:51:02 +0100

    030_full_backup_repair

--- Project/SQL feladatok/030_full_backup.sql
+++ Project/SQL feladatok/030_full_backup.sql
@@ -1,5 +1,5 @@
 -- 030 Készíts az INTERFACE adatbázisról egy FULL mentést, az alábbiak szerint:
 -- backup file neve: S:\SQLdata\INTERFACE.bak - mentés felülírja az azonos nevû media set-et!
 
-BACKUP DATABASE [INTERFACE] TO DISK = N': S:\SQLdata\INTERFACE.bak' 
+BACKUP DATABASE [INTERFACE] TO DISK = N'S:\SQLdata\INTERFACE.bak' 
 WITH NOFORMAT, INIT, NAME = N'INTERFACE-Full Database Backup', SKIP, NOREWIND, NOUNLOAD, STATS = 10
