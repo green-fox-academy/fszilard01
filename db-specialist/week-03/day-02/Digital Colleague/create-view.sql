be903b434241f2f317f61f2e99d52152b94bf538
fszilard01 <fekete.szilard76@gmail.com>
2022-05-02 08:53:17 +0200

    digitalC_w3d02

---
+++ db-specialist/week-03/day-02/Digital Colleague/create-view.sql
@@ -0,0 +1,11 @@
+﻿USE practice2022;
+GO
+
+CREATE OR ALTER VIEW HR.locationsview
+AS 
+SELECT d.name AS department_name, o.county_name AS county_name, o.city_name AS city_name, COUNT(e.id) AS employees_nr
+FROM HR.Departments d 
+	INNER JOIN HR.Locations l ON d.id=l.department_ID
+	INNER JOIN HR.Offices o ON l.offices_ID=o.place_id 
+	LEFT JOIN HR.Employees e ON l.id=e.location_id
+GROUP BY d.name, county_name, o.city_name;
---
+++ db-specialist/week-03/day-02/Digital Colleague/try-catch.sql
@@ -0,0 +1,10 @@
+﻿USE practice2022;
+
+BEGIN TRY
+    INSERT INTO HR.Departments (id, name) VALUES (88,'GreenFox')
+END TRY
+BEGIN CATCH
+	THROW 77777, 'primary key constraint was activated',1;
+END CATCH;
+
+SELECT 5
