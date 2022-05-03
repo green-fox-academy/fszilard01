a4501a977399af7027cfc3b6720da9b989d2e9f8
fszilard01 <fekete.szilard76@gmail.com>
2022-05-03 12:41:45 +0200

    digitalC_w4d01

---
+++ db-specialist/week-04/day-01/01_SelectStockOrdersProc.sql
@@ -0,0 +1,60 @@
+﻿
+USE WideWorldImporters;
+
+/* 
+    Create a stored procedure that returns all the orders for a given
+    Stock Item ID based on the Sales.OrderLines and Sales.Orders tables.
+    
+    The stored procedure should adhere to the following rules:
+     - accept a parameter for the Stock Item ID
+     - accept a parameter for limiting the number of rows returned
+     - should return the OrderLineID, OrderID and OrderDate
+     - order the results by OrderDate in descending order
+*/
+GO
+CREATE PROCEDURE Sales.customer_orders (@stockid int, @top int)
+ AS 
+   SELECT TOP(@top) OrderLineID,sor.OrderID,OrderDate
+   FROM Sales.OrderLines sor INNER JOIN Sales.Orders so ON sor.OrderID = so.OrderID
+   WHERE StockItemID = @stockid 
+   ORDER BY OrderDate DESC;
+GO	 
+
+EXEC Sales.customer_orders 18, 11;
+
+EXEC Sales.customer_orders @top=18, @stockid=11;
+
+/* 
+    Modify the stored procedure that you created.
+    
+    Add a default NULL value to the Stock Item ID parameter.
+
+    If the parameter value is NULL then return all the orders and orderlines.
+
+    If the parameter value is not NULL, then return only the orders for the
+    given Stock Item ID just as in the first version of your stored
+    procedure.  
+*/
+
+GO
+CREATE OR ALTER PROCEDURE Sales.customer_orders (@stockid INT = NULL, @top INT)
+ AS 
+ IF @stockid IS NOT NULL
+ BEGIN
+   SELECT TOP(@top) OrderLineID,sor.OrderID,OrderDate
+   FROM Sales.OrderLines sor INNER JOIN Sales.Orders so ON sor.OrderID = so.OrderID
+   WHERE StockItemID = @stockid 
+   ORDER BY OrderDate DESC
+END
+ELSE
+BEGIN
+	SELECT OrderLineID ,sor.OrderID ,OrderDate
+	FROM Sales.OrderLines sor INNER JOIN Sales.Orders so ON sor.OrderID = so.OrderID
+END;
+GO	
+
+EXEC Sales.customer_orders 18, 11;
+
+EXEC Sales.customer_orders @top=18, @stockid=NULL;
+
+EXEC Sales.customer_orders @top=18, @stockid=DEFAULT;
---
+++ db-specialist/week-04/day-01/02_InsertColorProc.sql
@@ -0,0 +1,60 @@
+﻿
+USE WideWorldImporters;
+
+/* 
+    Create a stored procedure that inserts new colors 
+    into the Warehouse.Colors table.
+
+    We want to add a new "Greenfox Green" color to the colors list!
+    
+    Before creating the stored procedure, analyze the Warehouse.Colors
+    table structure to see which columns are mandatory and which ones are
+    optional when adding a new order.
+
+    Make sure you use a proper ColorID for any new color you insert!
+
+    Right-click on the table in SSMS and choose "Script Table as..."
+    then "Create To" and then choose New Query Window.
+    You can then analyze the CREATE TABLE script for columns and constraints.
+
+    Check if there are DEFAULT constraints in the table that you can use!
+    
+    The stored procedure should adhere to the following rules:
+     - accept at least the color name as a parameter
+     - provide parameter default values where it is appropriate
+     - implement error handling
+     - print full error information about any error occurred
+     - return with a value of 0 if no error occurred
+     - return with a value of 2 if any error occurred
+     - return the newly inserted ColorID in an OUTPUT parameter  
+*/
+--	SELECT * FROM [WideWorldImporters].[Warehouse].[Colors]
+
+GO
+DROP PROCEDURE IF EXISTS Warehouse.AddColor;
+GO
+CREATE PROCEDURE Warehouse.AddColor(@colorname NVARCHAR(20) = NULL, @lasteditedby int = 1)
+AS
+BEGIN TRY
+	INSERT INTO Warehouse.Colors(ColorName, LastEditedBy)
+	OUTPUT INSERTED.ColorID
+	VALUES (@colorname, @lasteditedby)
+END TRY
+BEGIN CATCH
+    PRINT 'Error occurred!';
+    PRINT 'Error number: ' + CAST(ERROR_NUMBER() AS varchar(10));
+    PRINT 'Error message: ' + ERROR_MESSAGE();
+    PRINT 'Error state: ' + CAST(ERROR_STATE() AS varchar(10));
+    PRINT 'Error severity: ' + CAST(ERROR_SEVERITY() AS varchar(10));
+    PRINT 'Error line: ' + CAST(ERROR_LINE() AS varchar(10));
+    PRINT 'Error line: ' + ERROR_PROCEDURE();
+	RETURN 2;
+END CATCH;
+GO
+
+DECLARE @ret int;
+EXEC @ret = Warehouse.AddColor 'GF Green';
+PRINT 'Returned with ' + CAST(@ret AS varchar);
+
+-- DELETE FROM Warehouse.Colors WHERE ColorName LIKE 'GF Green'
+
